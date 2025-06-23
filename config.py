ddf796aeb39e48ea07fbf158f465d933f54a6ef4
khoant-cloudvn <khoant@ecloudvn.com>
2025-06-23 14:10:11 +0700

    config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     PORT = int(os.environ.get('PORT', 5000))
     
     # eBay API settings
-    EBAY_APP_ID = os.environ.get('KhoaNgoT-Applicat-SBX-3f7536b3f-b7ef7691')
-    EBAY_CLIENT_SECRET = os.environ.get('SBX-f7536b3fdc5b-a74e-4ff6-94b3-e86d')
+    EBAY_APP_ID = os.environ.get('EBAY_APP_ID')
+    EBAY_CLIENT_SECRET = os.environ.get('EBAY_CLIENT_SECRET')
     EBAY_OAUTH_URL = 'https://api.ebay.com/identity/v1/oauth2/token'
     EBAY_SEARCH_URL = 'https://api.ebay.com/buy/browse/v1/item_summary/search'
     EBAY_ITEM_URL = 'https://api.ebay.com/buy/browse/v1/item/'
@@ -23,4 +23,4 @@ class Config:
     
     # Security settings
     RATE_LIMIT = '100 per minute'
-    CORS_ORIGINS = ['*']  # Configure this based on your needs 
+    CORS_ORIGINS = ['https://chat.openai.com']  # Update to your GPT's domain in production
